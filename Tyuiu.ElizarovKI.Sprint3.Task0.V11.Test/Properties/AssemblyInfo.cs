b427db7ae20a8e65724c4e6d2db17a70e3e9d0c6
ElizarovKirill <144137165+ElizarovKirill@users.noreply.github.com>
2023-10-16 23:05:07 +0500

    Add project files.

---
+++ Tyuiu.ElizarovKI.Sprint3.Task0.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElizarovKI.Sprint3.Task0.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ElizarovKI.Sprint3.Task0.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd64fc84-5abe-442c-ad2b-e551d43675fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ElizarovKI.Sprint3.Task0.V11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ElizarovKI.Sprint3.Task0.V11.Lib;
+
+namespace Tyuiu.ElizarovKI.Sprint3.Task0.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Елизаров К. И. | ИИПб-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Елизаров Кирилл Игоревич | ИИПб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+            Console.WriteLine("* по формуле, при n = 5                                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 5;
+
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
